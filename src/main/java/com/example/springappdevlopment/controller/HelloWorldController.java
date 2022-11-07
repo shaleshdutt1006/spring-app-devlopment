abe0fd8029e2733ef97fbda927cefdfbdf2d86bb
shaleshdutt1006 <shaleshdutt1006@gmail.com>
2022-11-07 15:23:51 +0530

    Added: Welcome message

---
+++ src/main/java/com/example/springappdevlopment/controller/HelloWorldController.java
@@ -0,0 +1,8 @@
+package com.example.springappdevlopment.controller;
+
+
+import org.springframework.web.bind.annotation.*;
+
+
+
+
---
+++ src/test/java/com/example/springappdevlopment/SpringAppDevlopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springappdevlopment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppDevlopmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
