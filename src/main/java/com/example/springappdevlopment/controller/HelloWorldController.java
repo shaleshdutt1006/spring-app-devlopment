be557d48e55300b814fac3050d0da3b5b9edbb16
shaleshdutt1006 <shaleshdutt1006@gmail.com>
2022-11-07 15:26:38 +0530

    Added: Display Welcome message using Springframework

--- src/main/java/com/example/springappdevlopment/controller/HelloWorldController.java
+++ src/main/java/com/example/springappdevlopment/controller/HelloWorldController.java
@@ -3,6 +3,14 @@ package com.example.springappdevlopment.controller;
 
 import org.springframework.web.bind.annotation.*;
 
+@RestController
+
+@RequestMapping("/hello")
+public class HelloWorldController {
+    @GetMapping(value = {"", "/", "home"})
+    public String sayHello() {
+        return "Hello from Shalesh dutt";
+    }
 
 
-
+}
